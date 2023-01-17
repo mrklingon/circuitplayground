1da83966cf2ccbda528f4f7f9db540f2f52b1733
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-01-17 17:17:37 -0600

    Update dirship.py

--- dirship.py
+++ dirship.py
@@ -6,12 +6,15 @@ entdir =[
         [5,["Interrogation/Detention Area"]],
         [6,["transporter room 2"]],
         [7,["quarters"]],
-        [8,["quarters"],["battle bridge"]],
+        [8,["quarters","battle bridge"]],
         [9,["quarters"]],
         [10,["Ten Forward","Holodeck 5","science labs"]],
         [11,["Holodeck 3","Holodeck 2","Holodeck 4J","Cargo Bay 11"]],
         [12,["Biomedical lab","Nursery","Gymnasium","Science laboratory","Transporter Room 8"]],
         [13,["Jefferies tube","Shuttlebay 2","Shuttlebay 3"]],
         [14,["Transporter room","Primary care classroom","Conference Room"]],
+        [42,["main engineering","Science lab","engineering computer core access"]]
 
+         
     ]
+
