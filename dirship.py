4974f582836599ddb38464baab2d3faa56e32e68
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2023-01-17 11:46:04 -0600

    Add files via upload
    galaxy class directory - dirship.py
    navigate ship - ncc1701d.py

---
+++ dirship.py
@@ -0,0 +1,17 @@
+entdir =[
+        [1,["bridge","ready room","observation lounge"]],
+        [2,["sickbay"]],
+        [3,["dining room"]],
+        [4,["shuttlebay","cargo bays","ship's stores","holodeck"]],
+        [5,["Interrogation/Detention Area"]],
+        [6,["transporter room 2"]],
+        [7,["quarters"]],
+        [8,["quarters"],["battle bridge"]],
+        [9,["quarters"]],
+        [10,["Ten Forward","Holodeck 5","science labs"]],
+        [11,["Holodeck 3","Holodeck 2","Holodeck 4J","Cargo Bay 11"]],
+        [12,["Biomedical lab","Nursery","Gymnasium","Science laboratory","Transporter Room 8"]],
+        [13,["Jefferies tube","Shuttlebay 2","Shuttlebay 3"]],
+        [14,["Transporter room","Primary care classroom","Conference Room"]],
+
+    ]
---
+++ ncc1701d.py
@@ -0,0 +1,97 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+from adafruit_circuitplayground import cp
+import time
+import random
+import board
+from blinknum import *
+from dirship import *
+
+def saydeck(deck):
+    cp.play_file("ST/deck.wav")
+    showint(deck)
+def showroom(deck,room):
+    rm = entdir[deck][1][room]
+    if rm.find("olod") >= 0:
+        cp.play_file("ST/holodeck.wav")
+    if rm.find("bridge") >= 0:
+        cp.play_file("ST/bridge.wav")
+    if rm.find("anspor") >= 0:
+        cp.play_file("ST/transporter.wav")
+    if rm.find("uarter") >= 0:
+        cp.play_file("ST/quarters.wav")
+    if rm.find("orward") >= 0:
+        cp.play_file("ST/tenforward.wav")
+    if rm.find("lab") >= 0:
+        cp.play_file("ST/lab.wav")
+    if rm.find("uttle") >= 0:
+        cp.play_file("ST/shuttlebay.wav")
+    print(rm)
+
+def curfloor(deck):
+    return entdir[deck][0]
+
+def enterfloor(deck):
+#    cp.play_file("ST/Enterprise Doors.wav")
+    saydeck(curfloor(deck))
+#    print (entdir[deck][1])
+    return(entdir[deck][1])
+#cp.play_file("ST/Status report.wav")
+
+deck = 0
+room = 0
+
+while not cp.switch:
+    blinknum(1,blue)
+    
+cp.play_file("ST/Status report.wav")
+rooms = enterfloor(deck)
+
+turbo = False
+
+while True:
+    if cp.switch == False and turbo == False:
+        turbo = True
+        cp.play_file("ST/Enterprise Doors.wav")
+        saydeck(curfloor(deck))
+    
+    if cp.switch == True and turbo == True:
+        turbo = False
+        cp.play_file("ST/Enterprise Doors.wav")
+        rooms = enterfloor(deck)
+        
+    if not cp.switch and turbo == True:        
+        if cp.button_a:
+            deck = deck - 1
+            if deck < 0:
+                deck = 0
+            else:
+                enterfloor(deck)
+                
+        if cp.button_b:
+            deck = deck + 1
+            if deck == len(entdir):
+                deck = deck - 1
+            else:
+                enterfloor(deck)
+                
+    if cp.switch and turbo == False:
+        
+        if cp.button_a:
+            room = room - 1
+            if room < 0:
+                room = 0
+            showroom(deck,room)
+                
+        if cp.button_b:
+            room = room + 1
+            if room == len(rooms):
+                room = len(rooms) - 1
+            showroom(deck,room)
+
+    time.sleep(.1)
+                
+    
+
+        
