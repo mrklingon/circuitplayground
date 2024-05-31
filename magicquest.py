2453f0330e5aa80a168dde530297410048833bf3
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2024-05-30 20:21:07 -0500

    Update magicquest.py
    Updated to work with CP 9.x
    
    needs files: enemy, forest, treasure, weapons, names

--- magicquest.py
+++ magicquest.py
@@ -12,7 +12,7 @@ from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 from adafruit_hid.keycode import Keycode
 from digitalio import DigitalInOut, Direction, Pull
-
+from wise import *
 from adafruit_circuitplayground import cp
 
 red = (7,0,0)
@@ -22,72 +22,16 @@ blank = (0,0,0)
 
 races = ["human","elf","dwarf","kender", "drow"]
 classes = ["wizard", "thief","cleric","healer","warrior"]
-names =  ["Krargrouk Honorgrog","Kargroun Shadowbrew","Vomdoick Emberminer",
-    "Polo Littlefoot","Leutfrid Baggins","Fastolph Gardner","Alen Tormyar",
-    "Ascal Perbanise","Felaern Tortoris","Crestiennet of the Night",
-    "Thilrern Derorvo",
-    "Bheksomu Dreinzul",
-    "Khondui Bhugazel",
-    "Averitt the Heroic","Heimeri of the South"]
 
 destination = ["Alterwood Palace","Corftey Fortress","Clarn Castle","Wringcaster Citadel","The Eternal Haunt",
     "The Buried Burrows","The Ethereal Cells  ","Water Leopard Woods","Baxpool Covert","Kirkasing Woodland"]
 
-weapons = ["Soulsliver",
-    "Cryptic",
-    "Blackout",
-    "Doomguard",
-    "Windsong Idol",
-    "Thirsting Slicer",
-    "Isolated Copper Sabre",
-    "Pride's Steel Protector",
-    "Aetherius, Quickblade of Eternal Bloodlust",
-    "Treachery, Spine of Justice",
-    "Stinger, Claymore of the Burning Sun",
-    "Barbarian Iron Deflector",
-    "Mournblade, Gift of Ancient Power"]
 
-enemy = ["Phasething",
-    "Chaosling",
-    "Rottingstep",
-    "Boulderfang",
-    "Phantomman",
-    "Dreadcrackle",
-    "Soilserpent",
-    "Mornlich",
-    "The Lanky Statue",
-    "The Feathered Bane Jackal",
-    "The Black-Eyed Cinder Spider",
-    "The Tusked Venom Drake"]
 
 
 
 travel = [" travels to", " returns from", " explores within", " gets lost at"]
-treasure = ["Fortune's Horn",
-    "Worship Mirror",
-    "Insanity Boots",
-    "Sanctifying Ark",
-    "Roaring Scepter",
-    "Misty Satchel",
-    "Hailstorm Charm"]
 
-forest = ["Misty Bluff Wilds",
-    "Pleasant Brook Grove",
-    "Huge Wilds",
-    "Majestic Wilds",
-    "Short-Tailed Snail Woodland",
-    "Speckled Squirrel Grove",
-    "Faint Wood",
-    "Jagged Covert",
-    "Short-Tailed Treefrog Grove",
-    "Imperial Spider Covert",
-    "Mannear Wilds",
-    "Shipsons Wood",
-    "Alberboia Grove",
-    "Bois de la Camris",
-    "Fort du Bouluon",
-    "Fort de la Carcaveil",
-    "Fort de la Sarmont" ]
 
 problem = [ "Suddenly out of {frst} appears a {monster}", "{name} is surprised when {monster} attacks from {frst}",
     "A battle begins. The deadly {monster} attacks {name} after leaping out of {frst}!",
@@ -97,7 +41,11 @@ solution = [ "Stumbling badly, {name} somehow manages to find the {weapon} and d
              "Heroicly, {name} quickly banishes the creature to {forest} with the {weapon}.",
              "Even though {name} fails to find a weapon like {weapon}, the monster shuffles away to {forest}"]
 
-
+def getWD(fnm):
+    len = file_len(fnm)
+    line = wisdom(random.randrange(len),fnm)
+    line = line.strip("\n");
+    return (line)
 
 purpose = ["seeking", "discovering", "losing","finding"]
 cp.pixels.fill(blank)
@@ -110,7 +58,7 @@ kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(kbd)
 
 def player():
-    nm = random.choice(names)
+    nm = getWD("names")
     rc = random.choice(races)
     cl = random.choice(classes)
     return [nm,rc,cl]
@@ -127,7 +75,7 @@ def compthink(cycles):
         dur = random.uniform(.02,.125)
         cp.play_tone(freq,dur)
 
-# make all pin objects, make them inputs with pulldowns
+
 Done = False
 Player = player()
 
@@ -157,9 +105,9 @@ while not Done:
         prt("The " + Player[1]+ " " + Player[2] + " " + Player[0] )
         prt(random.choice(travel) )
         prt(random.choice(destination) )
-        prt(random.choice(purpose) + " "+ random.choice(treasure))
-        prt(random.choice(problem).format(name=Player[0],frst=random.choice(forest),monster=random.choice(enemy)))
-        prt(random.choice(solution).format(name=Player[0],forest=random.choice(forest),monster=random.choice(enemy),weapon=random.choice(weapons)))
+        prt(random.choice(purpose) + " "+ getWD("treasure"))
+        prt(random.choice(problem).format(name=Player[0],frst=getWD("forest"),monster=getWD("enemy")))
+        prt(random.choice(solution).format(name=Player[0],forest=getWD("forest"),monster=getWD("enemy"),weapon=getWD("weapons")))
 
 
         time.sleep(.25)
